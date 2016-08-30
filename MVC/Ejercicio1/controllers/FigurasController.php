5a592ac077886d87b1d8ee7440050b413d6e632b
Juan <rodriguezjuanmanuelar@gmail.com>
2016-08-30 11:17:56 -0300

    borrado de lineas comentadas

--- MVC/Ejercicio1/controllers/FigurasController.php
+++ MVC/Ejercicio1/controllers/FigurasController.php
@@ -16,15 +16,6 @@ class FigurasController
         $figuras =$this->modelo->getFiguras();
         $cantidad = count($figuras);
         usort($figuras,array("FiguraModel","comparator"));
-        /*for($i=0;$i< $cantidad;$i++){
-            for($j=0;$j< $cantidad;$j++){
-                if($figuras[$i]->getArea()>$figuras[$j]->getArea()){
-                    $menor=$figuras[$i];
-                    $figuras[$i]=$figuras[$j];
-                    $figuras[$j]=$menor;
-                }
-            };
-        };*/
         $this->vista->mostrar($figuras);
     }
 }
